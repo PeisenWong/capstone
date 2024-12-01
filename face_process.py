7491e10c826b674b93bc69ab78f56dcc8f4cafc0
PeisenWong <wongpeisen@gmail.com>
2024-12-01 21:37:17 +0800

    ndvn

--- face_process.py
+++ face_process.py
@@ -45,11 +45,11 @@ def process_frame(frame):
     
     face_names = []
     authorized_face_detected = False
+    name = "Unknown"
     
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
         
         # Use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
