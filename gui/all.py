fc45f7742dcf4bbd27b557e62d431cde0dd39530
PeisenWong <wongpeisen@gmail.com>
2024-12-10 21:16:00 +0800

    vnkj

--- gui/all.py
+++ gui/all.py
@@ -43,12 +43,12 @@ class CombinedPage(QWidget):
         top_layout = QHBoxLayout()
         self.ip_camera_label = QLabel("IP Camera Stream (Object Detection)")
         self.ip_camera_label.setAlignment(Qt.AlignCenter)
-        self.ip_camera_label.setFixedSize(400, 300)
+        self.ip_camera_label.setFixedSize(640, 480)
         self.ip_camera_label.setStyleSheet("border:1px solid black;")
 
         self.webcam_label = QLabel("Webcam Stream (Face Recognition)")
         self.webcam_label.setAlignment(Qt.AlignCenter)
-        self.webcam_label.setFixedSize(400, 300)
+        self.webcam_label.setFixedSize(200, 100)
         self.webcam_label.setStyleSheet("border:1px solid black;")
 
         top_layout.addWidget(self.ip_camera_label)
