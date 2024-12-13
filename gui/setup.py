c73f4914d2fcb663b0379076ab2b67186008a84d
PeisenWong <wongpeisen@gmail.com>
2024-12-13 23:03:09 +0800

    nbn

--- gui/setup.py
+++ gui/setup.py
@@ -9,7 +9,7 @@ from PyQt5.QtCore import QTimer, Qt, QPoint
 import sys
 
 # Load YOLOv8 model
-model = YOLO("models/trained.pt")
+model = YOLO("models/yolov8m.pt")
 
 class AdjustableImageLabel(QLabel):
     def __init__(self, parent=None):
