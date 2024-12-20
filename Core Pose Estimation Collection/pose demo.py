f26719e4c7f6f8d889afade52a1c7ba80662b859
PeisenWong <wongpeisen@gmail.com>
2024-12-20 16:27:21 +0800

    nfbn

---
+++ Core Pose Estimation Collection/pose demo.py
@@ -0,0 +1,45 @@
+import cv2
+from ultralytics import YOLO
+
+ip_cap = cv2.VideoCapture("rtsp://peisen:peisen@192.168.113.39:554/stream2")  # Use the first camera
+ip_cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+ip_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+if not ip_cap.isOpened():
+    raise RuntimeError("Failed to open camera")
+
+# Load our YOLO11 model
+model = YOLO("yolo11n-pose.pt")
+
+while True:
+    # Capture a frame from the camera
+    frame = ip_cap.read()
+    
+    # Run YOLO model on the captured frame and store the results
+    results = model.predict(frame, imgsz = 640)
+
+    # Output the visual detection data, we will draw this on our camera preview window
+    annotated_frame = results[0].plot()
+    
+    # Get inference time
+    inference_time = results[0].speed['inference']
+    fps = 1000 / inference_time  # Convert to milliseconds
+    text = f'FPS: {fps:.1f}'
+
+    # Define font and position
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_size = cv2.getTextSize(text, font, 1,2)[0]
+    text_x = annotated_frame.shape[1] - text_size[0] - 10  # 10 pixels from the right
+    text_y = text_size[1] + 10  # 10 pixels from the top
+
+    # Draw the text on the annotated frame
+    cv2.putText(annotated_frame, text, (text_x, text_y), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # Display the resulting frame
+    cv2.imshow("Camera", annotated_frame)
+
+    # Exit the program if q is pressed
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# Close all windows
+cv2.destroyAllWindows()
--- utils/image_capture.py
+++ utils/image_capture.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import time
 
 # Change this to the name of the person you're photographing
-PERSON_NAME = "9 Dec"  
+PERSON_NAME = "test"  
 
 def create_folder(name):
     dataset_folder = "../datasets/data"
