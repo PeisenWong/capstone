411c179c5c46c87d84efa5d5d783838bbbe9a6ea
PeisenWong <wongpeisen@gmail.com>
2024-12-23 10:39:11 +0800

    dndvn

--- gui/object_page.py
+++ gui/object_page.py
@@ -226,6 +226,10 @@ class ObjectPage(QWidget):
                                                                     X_person_br, Y_person_br)
                         inside_right_slow_vert = (side_right_foot_slow_vert < -1500)
 
+                        side_left_foot_slow_horz = point_side_of_line(X_slow_bl2, Y_slow_bl2, X_slow_br, Y_slow_br,
+                                            X_person_bl, Y_person_bl)
+                        inside_left_slow_horz = (side_left_foot_slow_horz < 0)
+
                         if  inside_right_slow_vert:
                             print(f"Person crosses slow zone vertical line! (Right side) {side_right_foot_slow_vert}")
                             cv2.putText(detection_frame, "INSIDE SLOW ZONE UP", (int(X_person_tl), int(Y_person_br)),
@@ -237,19 +241,17 @@ class ObjectPage(QWidget):
 
                         # 2) Horizontal slow line (bottom_left to bottom_right)
                         # Inside if > 0 means above the line
-                        
-                        side_left_foot_slow_horz = point_side_of_line(X_slow_bl2, Y_slow_bl2, X_slow_br, Y_slow_br,
-                                                                    X_person_bl, Y_person_bl)
-                        inside_left_slow_horz = (side_left_foot_slow_horz > 0)
 
                         if inside_left_slow_horz:
                             print(f"Person crosses slow zone horizontal line! (Above){side_left_foot_slow_horz}")
-                            # cv2.putText(detection_frame, "SLOW ZONE > 0!", (int(X_person_bl), int(Y_person_bl)),
-                            #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                        elif inside_left_slow_horz:
-                            print("Person is on slow zone horizontal boundary!")
                             cv2.putText(detection_frame, "INSIDE SLOW ZONE RIGHT!", (int(X_person_bl), int(Y_person_bl)),
                                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        elif inside_left_slow_horz:
+                            print("Person is on slow zone horizontal boundary!")
+                            # cv2.putText(detection_frame, "INSIDE SLOW ZONE RIGHT!", (int(X_person_bl), int(Y_person_bl)),
+                            #             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                            
+                        print(f"UP: {inside_right_slow_vert} RIGHT: {inside_left_slow_horz}")
 
                     # ---------------------
                     # Stop Zone Checks
